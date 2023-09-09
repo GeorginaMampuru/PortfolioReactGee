eeb1490aeda491d78694ef252ac6a893f8e616b2
Georgina <66384787+GeorginaMampuru@users.noreply.github.com>
2023-09-09 20:48:10 +0200

    About me section modified with a smooth scrolling section

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,19 +1,17 @@
-// import React from 'react';
-// import { BsTwitter, BsInstagram } from 'react-icons/bs';
+import React from 'react';
+import { BsLinkedin, BsGithub } from 'react-icons/bs';
 // import { FaFacebookF } from 'react-icons/fa';
 
-// const SocialMedia = () => (
-//   <div className="app__social">
-//     <div>
-//       <BsTwitter />
-//     </div>
-//     <div>
-//       <FaFacebookF />
-//     </div>
-//     <div>
-//       <BsInstagram />
-//     </div>
-//   </div>
-// );
+const SocialMedia = () => (
+  <div className="app__social">
+    <div>
+      <BsLinkedin />
+    </div>
+    <div>
+      <BsGithub />
+    </div>
+  
+  </div>
+);
 
-// export default SocialMedia;
+export default SocialMedia;
--- src/containers/About/About.jsx
+++ src/containers/About/About.jsx
@@ -1,10 +1,66 @@
-import React from 'react'
+import React from "react";
+import { motion } from "framer-motion";
+import { AppWrap, MotionWrap } from "../../wrapper";
+import { images } from "../../constants";
 import "./About.css";
+
+const abouts = [
+  {
+    title: "Web Development",
+    description:
+      "I've got passion for creating beautiful websites and web applications",
+    imgUrl: images.about01,
+  },
+  {
+    title: "Frontend Development",
+    description:
+      "I build user-facing features consisting of reusable components using React",
+    imgUrl: images.about02,
+  },
+  {
+    title: "Open Source",
+    description:
+      "I enjoy contributing to open source projects and sharing knowledge",
+    imgUrl: images.about03,
+  },
+  {
+    title: "UI Design",
+    description:
+      "I enjoy creating real websites and apps using Figma UI designs",
+    imgUrl: images.about04,
+  },
+];
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <>
+     <h2 className="head-text">About <span>Me</span></h2>
 
-export default About
+      <div className="app__profiles">
+        {abouts.map((about, index) => (
+          <motion.div
+            whileInView={{ opacity: 1 }}
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.5, type: "tween" }}
+            className="app__profile-item"
+            key={about.title + index}
+          >
+            <img src={about.imgUrl} alt={about.title} />
+            <h2 className="bold-text" style={{ marginTop: 20 }}>
+              {about.title}
+            </h2>
+            <p className="p-text" style={{ marginTop: 10 }}>
+              {about.description}
+            </p>
+          </motion.div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default AppWrap(
+  MotionWrap(About, "app__about"),
+  "about",
+  "app__whitebg"
+);
