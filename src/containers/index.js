c2aeb4ce6ca909453cbe50a32f823e3717693e69
GeorginaMampuru <georginamampuru@gmail.com>
2023-09-11 14:14:32 +0200

    Herosection aligned and responsive

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,14 +1,12 @@
 import About from './About/About';
-import Footer from './Footer/Footer';
-import Header from './Header/Header';
+// import Footer from './Footer/Footer';
 import Skills from './Skills/Skills';
 import Work from './Work/Work';
 import HeroSection from './HeroSection/HeroSection';
 
 export {
   About,
-  Footer,
-  Header,
+  // Footer,
   Skills,
   Work,
   HeroSection,
