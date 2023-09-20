5ac2bdc82880772e059977eec789b4fc9f612c4a
GeorginaMampuru <georginamampuru@gmail.com>
2023-09-20 15:55:50 +0200

    Adding the email js

--- src/containers/HeroSection/HeroSection.jsx
+++ src/containers/HeroSection/HeroSection.jsx
@@ -1,9 +1,7 @@
-
 // eslint-disable-next-line no-unused-vars
 import React, { useEffect, useRef } from 'react';
 import Typed from 'typed.js';
 import './HeroSection.css';
-
 
 const HeroSection = () => {
   const typingRef = useRef(null);
@@ -25,27 +23,31 @@ const HeroSection = () => {
 
   return (
     <section className="home" id="home">
-      <div className="content-container">
-        <div className="content">
-          <h2>Hi There,<br/> I am Georgina  <span>Mampuru</span></h2>
-          <p>i am into <span className="typing-text" ref={typingRef}></span></p>
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-6">
+            <div className="content">
+              <h2>Hi There,<br/> I am Georgina  <span>Mampuru</span></h2>
+              <p>i am into <span className="typing-text" ref={typingRef}></span></p>
 
-          <div className="box-container">
-            <div className="box">
-              <p><span>Email:</span> georginamampuru@gmail.com</p>
+              <div className="box-container">
+                <div className="box">
+                  <p><span>Email:</span> georginamampuru@gmail.com</p>
+                </div>
+              </div>
+              <div className="resumebtn">
+                <a onClick={() => window.open('https://drive.google.com/file/d/1nvHgyyLs2VMWoS4BssG-TidolVHPAsG7/view?usp=sharing')} className="btn">
+                  <button>Download CV</button>
+                </a>
+              </div>
             </div>
           </div>
-          <div className="resumebtn">
-          <a onClick={() => window.open('https://drive.google.com/file/d/1nvHgyyLs2VMWoS4BssG-TidolVHPAsG7/view?usp=sharing')} className="btn">
-              <button>DownloadCV</button>
-            
-            </a>
+          <div className="col-lg-6">
+            <div className="image">
+              {/* Add your image source here */}
+              <img src="./profileHero.png" alt="Georgina Mampuru" className="img-fluid" />
+            </div>
           </div>
-          
-        
-        </div>
-        <div className="image">
-          <img draggable="false" className="tilt" src="./profileHero.png" alt="" />
         </div>
       </div>
     </section>
